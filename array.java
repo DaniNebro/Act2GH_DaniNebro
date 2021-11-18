87333d9c28b26e03c70475e7bfaaf4a9f3d131ef
DaniNebro <a_daniel.nebro.ocana@iespablopicasso.es>
2021-11-18 14:25:23 +0100

    Array de números aleatorios

---
+++ array.java
@@ -0,0 +1,20 @@
+
+public class array {
+
+	public static void main(String[] args) {
+		
+		int aleatorio = 0;
+		
+		int [] array = new int[20];
+			
+		for(int i = 0; i < array.length; i++) {
+			
+			aleatorio = (int)(Math.random()*10);
+			
+			array[i] = aleatorio;
+			
+		}
+		
+	}
+
+}
